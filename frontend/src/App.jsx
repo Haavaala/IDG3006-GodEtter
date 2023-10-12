9526b20d5abf3935d40111d822912d4baaf2a95d
Nicolas <94457574+nxtseq@users.noreply.github.com>
2023-10-12 12:50:35 +0200

    Added axios to front-end and created a data test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,36 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    retrieveData();
+  }, []);
+
+  const retrieveData = () => {
+    axios.get(import.meta.env.VITE_API_URL).then((res) => {
+      setData(res.data.data);
+      console.log(data);
+    });
+  };
+  if (!data) return null;
 
   return (
     <>
-      <h1>Hallo Jeg Digger React Und PHPs!</h1>
+      <h1>Fridge:</h1>
+      {data.map((i) => {
+        return (
+          <div>
+            <h3>{i.name != "" ? i.name : "Unknown"}</h3>
+            <p>Brand: {i.brand != "" ? i.brand : "Unknown"}</p>
+            <p>Amount: {i.amount}</p>
+            <p>Added: {i.date}</p>
+          </div>
+        );
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
