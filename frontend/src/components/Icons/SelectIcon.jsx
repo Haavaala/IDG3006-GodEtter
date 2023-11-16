974e04cfc388afe220b0b16c6640d843694bd098
ingring <ingrid.gladheim@student.uib.no>
2023-11-16 11:32:59 +0100

    more icons

--- frontend/src/components/Icons/SelectIcon.jsx
+++ frontend/src/components/Icons/SelectIcon.jsx
@@ -3,6 +3,7 @@ import './icons.css';
 import Dessert from './Dessert';
 import Beverage from './Beverage';
 import Uncategorized from './Uncategorized';
+import Dairy from './Dairy';
 
 export default function SelectIcon({category, size}) {
 
@@ -12,6 +13,7 @@ export default function SelectIcon({category, size}) {
             {(category === 'Uncategorized')?<Uncategorized size={size} />:''}
             {(category === 'Dessert')?<Dessert size={size}/>:''}
             {(category === 'Drikke')?<Beverage size={size}/>:''}
+            {(category === 'Meieri og egg')?<Dairy size={size}/>:''}
         </>
     )
 }
