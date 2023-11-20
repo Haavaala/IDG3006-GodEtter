e39813558c9fa1cbe99cc8dc42d894ce4d6d2ce8
ingring <ingrid.gladheim@student.uib.no>
2023-11-20 19:10:37 +0100

    background blur and no scroll

--- frontend/src/components/InventoryCard/InventoryCard.jsx
+++ frontend/src/components/InventoryCard/InventoryCard.jsx
@@ -1,14 +1,11 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./inventoryCard.css";
-import Dessert from "../Icons/Dessert";
 import NewSticker from "./NewSticker";
-import axios from "axios";
 import instance from "../../instance";
 
 export default function InventoryCard({
   volume,
   title,
-  num,
   iconSmall,
   iconBig,
   dateBestBefore,
@@ -40,10 +37,18 @@ export default function InventoryCard({
 
     if (isDialogOpen) {
       document.addEventListener('mousedown', handleClickOutside);
+      document.querySelectorAll('.disable-pointer-events').forEach((element) => {
+        element.style.pointerEvents = 'none';
+      });
+      document.body.style.overflow = 'hidden';
     }
 
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
+      document.querySelectorAll('.disable-pointer-events').forEach((element) => {
+        element.style.pointerEvents = 'auto';
+      });
+      document.body.style.overflow = 'auto';
     };
   }, [isDialogOpen]);
   // const dialogRef = useRef(null);
@@ -106,7 +111,7 @@ export default function InventoryCard({
     return formattedDate;
   };
 
-  const FormatTime = (date) => {
+  const formatTime = (date) => {
     const dateObject = new Date(date);
 
     const hours = dateObject.getHours();
@@ -168,7 +173,7 @@ export default function InventoryCard({
 
   return (
     <>
-      <div className="inventoryCard__container" onClick={openDialog}>
+      <div className="inventoryCard__container disable-pointer-events" onClick={openDialog}>
         {setNewSticker('small')}
         <div className="inventoryCard__firstPartContainer">
           <div className="inventoryCard__icon">{iconSmall ? iconSmall : ""}</div>
@@ -384,14 +389,38 @@ export default function InventoryCard({
           </div>
         </div>
         <div className="inventoryCard__stroke--horizontal" />
-        <div className="inventoryDialog__scannedAndDelete">
-          <p className="inventoryDialog__scannedAndDelete--scanned">
-            Scannet <span className="smallp">{formatDate(dateScanned)}</span>
+        <div className="inventoryDialog__scannedAndModify">
+          <p className="inventoryDialog__scanned">
+            Scannet <span className="smallp">{formatDate(dateScanned)} kl. {formatTime(dateScanned)}</span>
           </p>
-          <button onClick={handleDeleteItem}>Slett varene</button>
+          <div className="inventoryDialog__modify">
+            <button className="edit" onClick={handleDeleteItem}>
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18.3334 8.75033V10.0003C18.3334 13.9287 18.3334 15.8929 17.113 17.1133C15.8926 18.3337 13.9285 18.3337 10.0001 18.3337C6.07171 18.3337 4.10752 18.3337 2.88714 17.1133C1.66675 15.8929 1.66675 13.9287 1.66675 10.0003C1.66675 6.07195 1.66675 4.10777 2.88714 2.88738C4.10752 1.66699 6.07171 1.66699 10.0001 1.66699H11.2501" stroke="#1A1A1A" stroke-linecap="round"/>
+            <path d="M13.8768 2.87954L14.4175 2.33886C15.3133 1.44304 16.7657 1.44304 17.6615 2.33886C18.5574 3.23469 18.5574 4.68711 17.6615 5.58294L17.1209 6.12362M13.8768 2.87954C13.8768 2.87954 13.9444 4.02849 14.9581 5.04226C15.9719 6.05604 17.1209 6.12362 17.1209 6.12362M13.8768 2.87954L8.90606 7.85027C8.56938 8.18694 8.40105 8.35528 8.25627 8.5409C8.08549 8.75985 7.93908 8.99676 7.81961 9.24742C7.71834 9.45992 7.64306 9.68577 7.49249 10.1375L7.01043 11.5837M17.1209 6.12362L12.1501 11.0943C11.8135 11.431 11.6451 11.5994 11.4595 11.7441C11.2406 11.9149 11.0037 12.0613 10.753 12.1808C10.5405 12.2821 10.3146 12.3573 9.86294 12.5079L8.41675 12.99M8.41675 12.99L7.48099 13.3019C7.2587 13.376 7.01363 13.3181 6.84795 13.1525C6.68227 12.9868 6.62441 12.7417 6.69851 12.5194L7.01043 11.5837M8.41675 12.99L7.01043 11.5837" stroke="#1A1A1A"/>
+            </svg>
+            <p>
+              Rediger vare
+            </p>
+
+            </button>
+            <button className="delete" onClick={handleDeleteItem}>
+            <p>
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M17.0832 5H2.9165" stroke="#1A1A1A" stroke-linecap="round"/>
+                <path d="M15.6946 7.08301L15.3113 12.8323C15.1638 15.0447 15.09 16.1509 14.3692 16.8253C13.6483 17.4997 12.5397 17.4997 10.3223 17.4997H9.67787C7.46054 17.4997 6.35187 17.4997 5.63103 16.8253C4.91019 16.1509 4.83644 15.0447 4.68895 12.8323L4.30566 7.08301" stroke="#1A1A1A" stroke-linecap="round"/>
+                <path d="M7.91675 9.16699L8.33342 13.3337" stroke="#1A1A1A" stroke-linecap="round"/>
+                <path d="M12.0834 9.16699L11.6667 13.3337" stroke="#1A1A1A" stroke-linecap="round"/>
+                <path d="M5.41675 5C5.46331 5 5.4866 5 5.50771 4.99947C6.19391 4.98208 6.79927 4.54576 7.03276 3.90027C7.03994 3.88041 7.04731 3.85832 7.06203 3.81415L7.14294 3.57143C7.212 3.36423 7.24654 3.26063 7.29234 3.17267C7.47509 2.82173 7.8132 2.57803 8.20392 2.51564C8.30186 2.5 8.41106 2.5 8.62947 2.5H11.3707C11.5891 2.5 11.6983 2.5 11.7962 2.51564C12.187 2.57803 12.5251 2.82173 12.7078 3.17267C12.7536 3.26063 12.7882 3.36423 12.8572 3.57143L12.9381 3.81415C12.9528 3.85826 12.9602 3.88042 12.9674 3.90027C13.2009 4.54576 13.8063 4.98208 14.4925 4.99947C14.5136 5 14.5368 5 14.5834 5" stroke="#1A1A1A"/>
+              </svg>
+              <span>Slett varen</span>
+            </p>
+            </button>
+          </div>
         </div>
       </dialog>
       )}
+    <div className={`blur-background ${isDialogOpen ? '' : 'hidden'}`} />
     </>
   );
 }
